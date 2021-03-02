df1336f499896ef15ed7728581092b54e6a9b671
Pan Ziyue <1763181+d3lta-v@users.noreply.github.com>
2021-03-02 22:33:09 +0800

    cookies are working

--- src/IPC/SystemInfoChannel.ts
+++ src/IPC/SystemInfoChannel.ts
@@ -3,9 +3,12 @@ import {IpcMainEvent, BrowserWindow, dialog, app} from 'electron';
 import fs from 'fs';
 import path from 'path';
 import {IpcRequest} from "../shared/IpcRequest";
-
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axiosCookieJarSupport from 'axios-cookiejar-support';
+import tough from 'tough-cookie';
 
+axiosCookieJarSupport(axios);
+const cookieJar = new tough.CookieJar();
 interface WJsonResponse {
   status?: string;
   message?: string;
@@ -42,6 +45,11 @@ export class SystemInfoChannel implements IpcChannelInterface {
       return;
     }
 
+
+    // Create base
+    // const BASE = Buffer.from("aHR0cHM6Ly93d3cuaWJtLmNvbQ==", "base64").toString('ascii');
+    // const ax = axios.create({baseURL: BASE});
+
     // Web stuff goes here
     const pay1 = Buffer.from("aHR0cHM6Ly93d3cuaWJtLmNvbS9kZW1vcy9saXZlL3R0cy1kZW1vL2FwaS90dHMvc3RvcmU=", "base64").toString('ascii');
     const pay2 = Buffer.from("aHR0cHM6Ly93d3cuaWJtLmNvbS9kZW1vcy9saXZlL3R0cy1kZW1vL2FwaS90dHMvbmV3U3ludGhlc2l6ZT92b2ljZT1lbi1VU19NaWNoYWVsVjNWb2ljZSZpZD1iZDk4ZTBlMi1jMTlkLTQzNDgtOTEyZC00ZWE4N2NjZGM4ZjI=", "base64").toString('ascii');
@@ -50,21 +58,19 @@ export class SystemInfoChannel implements IpcChannelInterface {
       method: 'post',
       url: pay1,
       headers: { 
-        'Content-Type': 'application/json',
-        'Cookie': '_abck=90B8E18474DEC2B6D733D2D798CD5F6C~-1~YAAQVN6bfGBRuNt3AQAA0haL7AVq96GKI+lnQ9lLYZAIk/WaHb9R0mCZIqKck6boKqwanYSOjZn0iM7S1VsdV+A+E0R53Rlc0l0eyo3xQRSWtv7XEled3MWzcOhGH3g8El4Zy7Csg6jLLtg4WejsdeebFgEDKcwxVYjjlfpNOoZXGHv93DJNilSc3wuucmtMnM0aWxP9FwQJaKO8bTm3zU04AVO3rXJcSinG8UhVq2NQvORflTN0v4+4yz5OuSUjNV0HjAgw7Ptk0OjGqdXpN2Y9XXmgFLMk6PisYYwWZ7mpmRNeD38GWhL3GLWxBK6qDs3uuZPnB07lsXpaUyi7Z4EbllVXPTPGRcC2O8wjYA==~-1~-1~-1; ak_bmsc=8B6090BC76E80DE79B9C52C668461E1A7C9BDE54F57F00001A8E3C604A9EA66A~plEJwb0K+JIN50Pris2SggbtkJPPlRUyStIMJ9a6NeK8MEkLJ0WIxJ/0ya4l/q7NzbaupeiSFnqa6+sqaa+F/5R0W0EZ9Z4SD7MYf05n06nyQcxzFqeZ63ngnRk2uAMPE+iczO8n9rns5wzs5ImahkGsPQeN9eFG9p87VAWm4LLGZrYu/oKXWJDkVUxGusCpnVefAzrSmninbbJnrEHaMsfwJiXiREwmNsBxuIBJ1Ugeg=; bm_sz=A8ADC40CC5CF884A90E0ECF36A72DA2B~YAAQVN6bfF9RuNt3AQAA0haL7AqtSp2RBe/XqgkwB1sSap0uWnv5Dwi0R1ICvybUumeRWR9Jj/8V+snAscz8i8Vd9tNTT23SdlylpBU6mWihwE3RD6GKj25pSv0gnr6oKziFMUJ+6ghT37E4KLyUc0GzD0hcaLkExzGAt8M+kBfbaxePKFDMEpQBKOS7; 848c305b58c818e6f2a11125da17c831=77f8646a12f2e6b917d237fe1439239f; tts-demo=s%3A1hhMbBK4arAwyLfVPEqf_Enqd-eV0fRZ.x39b%2Bp08nTFh75kDn%2F0E4h2Nc3WURIbDw1eDAWRW3JA'
+        'Content-Type': 'application/json'
       },
+      jar: cookieJar,
       data : request1Body,
       withCredentials: true
     };
     const request2Config: AxiosRequestConfig = {
       method: 'get',
       url: pay2,
-      headers: {
-        'Cookie': '_abck=90B8E18474DEC2B6D733D2D798CD5F6C~-1~YAAQVN6bfGBRuNt3AQAA0haL7AVq96GKI+lnQ9lLYZAIk/WaHb9R0mCZIqKck6boKqwanYSOjZn0iM7S1VsdV+A+E0R53Rlc0l0eyo3xQRSWtv7XEled3MWzcOhGH3g8El4Zy7Csg6jLLtg4WejsdeebFgEDKcwxVYjjlfpNOoZXGHv93DJNilSc3wuucmtMnM0aWxP9FwQJaKO8bTm3zU04AVO3rXJcSinG8UhVq2NQvORflTN0v4+4yz5OuSUjNV0HjAgw7Ptk0OjGqdXpN2Y9XXmgFLMk6PisYYwWZ7mpmRNeD38GWhL3GLWxBK6qDs3uuZPnB07lsXpaUyi7Z4EbllVXPTPGRcC2O8wjYA==~-1~-1~-1; ak_bmsc=8B6090BC76E80DE79B9C52C668461E1A7C9BDE54F57F00001A8E3C604A9EA66A~plEJwb0K+JIN50Pris2SggbtkJPPlRUyStIMJ9a6NeK8MEkLJ0WIxJ/0ya4l/q7NzbaupeiSFnqa6+sqaa+F/5R0W0EZ9Z4SD7MYf05n06nyQcxzFqeZ63ngnRk2uAMPE+iczO8n9rns5wzs5ImahkGsPQeN9eFG9p87VAWm4LLGZrYu/oKXWJDkVUxGusCpnVefAzrSmninbbJnrEHaMsfwJiXiREwmNsBxuIBJ1Ugeg=; bm_sz=A8ADC40CC5CF884A90E0ECF36A72DA2B~YAAQVN6bfF9RuNt3AQAA0haL7AqtSp2RBe/XqgkwB1sSap0uWnv5Dwi0R1ICvybUumeRWR9Jj/8V+snAscz8i8Vd9tNTT23SdlylpBU6mWihwE3RD6GKj25pSv0gnr6oKziFMUJ+6ghT37E4KLyUc0GzD0hcaLkExzGAt8M+kBfbaxePKFDMEpQBKOS7; 848c305b58c818e6f2a11125da17c831=77f8646a12f2e6b917d237fe1439239f; tts-demo=s%3A1hhMbBK4arAwyLfVPEqf_Enqd-eV0fRZ.x39b%2Bp08nTFh75kDn%2F0E4h2Nc3WURIbDw1eDAWRW3JA'
-      },
+      jar: cookieJar,
       responseType: 'arraybuffer',
       responseEncoding: 'binary',
-      // withCredentials: true
+      withCredentials: true
     };
 
     const getSpeechFile = async () => {
